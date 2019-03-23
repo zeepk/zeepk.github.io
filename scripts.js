e87a36c95ce1f1dd79fb92f21b2e8542ed03c6f8
Matt Hughes <matthughes@Matts-MacBook-Pro.local>
2019-03-22 18:16:28 -0700

    reformatting

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 populate()
 var data_array = [
-    [0, 'Overall']
+    [0, 'Overall'],
     [1, 'Attack'],
     [2, 'Defence'],
     [3, 'Strength'],
@@ -58,123 +58,139 @@ var data_array = [
     [55, 'Hard Clues'],
     [56, 'Elite Clues'],
     [57, 'Master Clues'],
-    ];
-    // var skill_map = new Map(skill_array);
-    // console.log(skill_array[5][1])
-    
+];
+// var skill_map = new Map(skill_array);
+// console.log(skill_array[5][1])
 
-function populate(){
-        // player_name = document.getElementById("searchbox").value
-        player_name = "zee pk"
-        player_name_plus = player_name.replace("_", "+")
-        player_name_plus =  player_name_plus.replace(" ", "+")
 
-        console.log(player_name)
-        proxyurl = "https://cors-anywhere.herokuapp.com/";
-        // console.log(document.getElementById("searchbox").value)
-        $.ajax({
-        dataType:'text',
+function populate() {
+    // player_name = document.getElementById("searchbox").value
+    player_name = "zee_pk"
+    player_name_plus = player_name.replace("_", "+")
+    player_name_plus = player_name_plus.replace(" ", "+")
+
+    console.log(player_name)
+    proxyurl = "https://cors-anywhere.herokuapp.com/";
+    // console.log(document.getElementById("searchbox").value)
+    $.ajax({
+        dataType: 'text',
         url: proxyurl + "https://secure.runescape.com/m=hiscore/index_lite.ws?player=" + player_name,
         type: 'GET',
+        success: function (res) {
+            loadData(res, player_name)
+
+
+        },
+        error: function (request, error) {
+            console.log("Request: " + JSON.stringify(request))
+        }
+    });
+    $.ajax({
+        dataType:'json',
+        url: proxyurl + "https://apps.runescape.com/runemetrics/profile/profile?user=" + player_name + "&activities=20",
+        type: 'GET',
         success: function(res) {
-                console.log(res)
-                
+                loadActivities(res);
             
         },
         error : function(request, error) {
             console.log("Request: "+JSON.stringify(request))
         }
     });
-    }
+}
 // Create the XHR object.
 
 
 
-function loadData(res){
+function loadData(res, player_name) {
+    console.log("running data for " + player_name)
     var skills = {}
     var minigames = {}
-    var data_array = res.split("\n")
-    for(i=0;i<28;i++){
-        var individual_skill_array = data_array[i].split(",")
+    var temp_data_array = res.split("\n")
+    for (i = 0; i < 28; i++) {
+        var individual_skill_array = temp_data_array[i].split(",")
+        xp = individual_skill_array[2]
+        xp = parseInt(xp, 10)
         skills[i] = {
             id: i,
-            name: skill_array[i],
+            name: data_array[i][1],
             rank: individual_skill_array[0],
             level: individual_skill_array[1],
-            xp: individual_skill_array[2],
-        }    
+            xp: xp.toLocaleString('en')
+        }
+
     }
-    for(i=28;i<57;i++){
-        var individual_minigame_array = data_array[i].split(",")
+    for (i = 28; i < 57; i++) {
+        var individual_minigame_array = temp_data_array[i].split(",")
         minigames[i] = {
             id: i,
-            name: skill_array[i],
-            score: minigame_skill_array[0],
-            rank: minigame_skill_array[1],
+            name: data_array[i][1],
+            score: individual_minigame_array[0],
+            rank: individual_minigame_array[1],
 
-        }    
+        }
     }
-    for(i=0;i<skills.length;i++){
-        console.log(skills[i].name + ": " + skills[i].xp)
+    for (i = 0; i < 28; i++) {
+        document.getElementById(i + 'name').innerHTML = skills[i].name
+        document.getElementById(i + 'level').innerHTML = skills[i].level
+        document.getElementById(i + 'rank').innerHTML = skills[i].rank
+        document.getElementById(i + 'xp').innerHTML = skills[i].xp
+
     }
 
-    var xp = res_dict['totalxp']
-    xp = xp.toLocaleString('en')
-    document.getElementById("username").innerHTML = document.getElementById("searchbox").value
- 
+
+
     document.getElementById("avatar").src = "http://secure.runescape.com/m=avatar-rs/" + player_name + "/chat.png"
 
 
-    
+
     var stat_table = document.getElementById("stat-table")
-    var activity_table = document.getElementById("activity-table")
     stat_table.style.display = "block"
-    activity_table.style.display = "block"
-    for(j=0;j<20;j++){
-        // console.log(res_dict['activities'][j]['text'])
-        document.getElementById('activity'+j).innerHTML = res_dict['activities'][j]['text']
-        document.getElementById('activity'+j).setAttribute("title", res_dict['activities'][j]['date'])
-    }
+
 }
-function create_table(){
+
+function loadActivities (res_dict){
+    for (j = 0; j < 20; j++) {
+        // console.log(res_dict['activities'][j]['text'])
+        document.getElementById('activity' + j).innerHTML = res_dict['activities'][j]['text']
+        document.getElementById('activity' + j).setAttribute("title", res_dict['activities'][j]['date'])
+    }
+    var activity_table = document.getElementById("activity-table")
+    activity_table.style.display = "block"
+
+}
+
+function create_table() {
     var table = document.getElementById("stat-table")
     // var overall = table.appendChild(document.createElement("td"))
     // overall.id = "overall"
-    var oa = table.appendChild(document.createElement("tr"))
-    
-    var x = table.appendChild(document.createElement("td"))
-    x.id = 'overallname'
-    x.style = "font-weight:bold;"
-    var t = table.appendChild(document.createElement("td"))
-    t.id = 'overalllevel'
-    t.style = "font-weight:bold;"
-    var y = table.appendChild(document.createElement("td"))
-    y.id = 'overallxp'
-    y.style = "font-weight:bold;"
-    var z = table.appendChild(document.createElement("td"))
-    z.id = 'overallrank'
-    z.style = "font-weight:bold;"
-        for(i=0;i<27;i++){
-            table.appendChild(document.createElement("tr"))
-            var x = table.appendChild(document.createElement("td"))
-            x.id = skill_array[i][1] + 'name'
-            var t = table.appendChild(document.createElement("td"))
-            t.id = skill_array[i][1] + 'level'
-            var y = table.appendChild(document.createElement("td"))
-            y.id = skill_array[i][1] + 'xp'
-            var z = table.appendChild(document.createElement("td"))
-            z.id = skill_array[i][1] + 'rank'
-            // table.appendChild(document.createElement("tr"))
-            // table.appendChild(document.createElement("td")).innerHTML = stats_dict[i].skill_name
-            // table.appendChild(document.createElement("td")).innerHTML = stats_dict[i].skill_xp
-            // table.appendChild(document.createElement("td")).innerHTML = stats_dict[i].skill_rank
+ 
+    for (i = 0; i < 28; i++) {
+        table.appendChild(document.createElement("tr"))
+        var x = table.appendChild(document.createElement("td"))
+        x.id = i + 'name'
+        var t = table.appendChild(document.createElement("td"))
+        t.id = i + 'level'
+        var y = table.appendChild(document.createElement("td"))
+        y.id = i + 'xp'
+        var z = table.appendChild(document.createElement("td"))
+        z.id = i + 'rank'
+        // table.appendChild(document.createElement("tr"))
+        // table.appendChild(document.createElement("td")).innerHTML = stats_dict[i].skill_name
+        // table.appendChild(document.createElement("td")).innerHTML = stats_dict[i].skill_xp
+        // table.appendChild(document.createElement("td")).innerHTML = stats_dict[i].skill_rank
 
-        }
+    }
+    document.getElementById('0name').style = "font-weight:bold;"
+    document.getElementById('0name').style = "font-weight:bold;"
+    document.getElementById('0name').style = "font-weight:bold;"
+    document.getElementById('0name').style = "font-weight:bold;"
+
     var activity = document.getElementById("activity-table")
-        for(i=0;i<20;i++){
-            activity.appendChild(document.createElement("tr"))
-            var n = activity.appendChild(document.createElement("td"))
-            n.id = 'activity' + i
-        }
+    for (i = 0; i < 20; i++) {
+        activity.appendChild(document.createElement("tr"))
+        var n = activity.appendChild(document.createElement("td"))
+        n.id = 'activity' + i
+    }
 
 }
